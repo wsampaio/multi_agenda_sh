d447e3ea2471b43d8905cdfbe410ecb3fd8a46d0
WELL Sampaio <wsampaio@yahoo.com>
2018-11-15 13:38:12 -0200

    alterado endereco pasta db

--- db.sh
+++ db.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ -e "../storage/downloads/financeiro.db" ]
+if [ -e "../storage/downloads/DB-multi_agenda/financeiro.db" ]
 then
-	db="../storage/downloads"
+	db="../storage/downloads/DB-multi_agenda"
 else
 	db="../DB-multi_agenda"
 fi
